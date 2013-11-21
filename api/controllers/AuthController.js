77756883904b7d578dfffa998ed0b33b69b4633d
Sean Kennedy <sean@spareboxdesign.com>
2013-11-21 22:10:19 +1000

    updated auth controller

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,5 +1,5 @@
 /**
- * AuthmarkController
+ * AuthController
  *
  * @module      :: Controller
  * @description	:: A set of functions called `actions`.
@@ -40,126 +40,117 @@ module.exports = {
 		/** This should all be policy middleware to catch every API endpoint ***/
 		
 		/** Using the API key flow might be a security issue on the client side. Passing the API key back to the client for use in a cookie is bad.
-			The alternative is hitting Facebook to authenticate every request to the API. Or  ***/
+			The alternative is hitting Facebook to authenticate every request to the API.  ***/
 			
 		/** In the case of a single page app this route could be loaded at the start of the app bootstrap and set the global key for the rest of the app's calls. 
 			With page reloads this route will be called for every request. Work in progress. **/
-		
-		var api_key = '',
-			user = {};
-		
-		// See if the request is using an API key
-		if (!api_key) {
 			
-			// Check for Facebook ID and Access token params
-			if (access_token && facebook_id) {
-				
-				// Lookup DB for the User
-				user = db.lookup.facebook_id;
-				
-				if (!user) {
-					
-					// User doesn't exist yet, we need to verify from Facebook this is a legit request, this is now a registration as the user
-					// was not in the DB already. Best to include the API's Facebook app secret key with these calls to verify the token originates from our app
-					
-					var user_data = graph.api.call.withapp.secret;
-					
-					if (!err) {
-						
-						// User is legit with legit token, generate an API key from some credentials and add them to the DB
-						// Optional: Store the Facebook token, no real need though
-						
-						api_key = newapi_key;
-						
-						user.add.to.db;
-						
-						// Send the success response to the client with the new API key to use
-						
-					} else {
-						
-						// Token isn't verified, send back an error the client can use
-						
-					}
-					
-				} else {
-					
-					// User exists already in our database
-					// Query Facebook to check the Facebook token is legit as the Facebook ID is easy to fake
-					// Best to include the API's Facebook app secret key with these calls to verify the token originates from our app
-					
-					var user_data = graph.api.call.withapp.secret;
-					
-					if (!err) {
-						
-						// User is legit, lookup their API key and send it back to the client for use
-						
-					} else {
-						
-						// Token isn't verified, send back an error the client can use
-						
-					}
-					
-				}
-				
-			} else {
-				
-				// Insufficient params supplied, send back an error response the client can use
-				
-			}
-			
-		} else {
-			
-			// They have an API key, might not be a real one though
-			// This shouldn't happen as the /auth route shouldn't be used if the client already has an API key
-			
-			// Probably best to spit the API key back at the client as this route is only for authenticating
-			
-			// Optional: check the API key against the DB, if nothing is found send back an error, else, spit the key back.
-			// Not much use in this option and it's adding to server load. It might prevent the client setting a fake key for use in the app though.
-			
-		}
+		// Set the vars
+		var facebook_id = '',
+			access_token = '',
+			graph_api = 'https://graph.facebook.com/me?access_token=';
 		
 		// Get Params
 		if (req.param('facebook_id')) {
-			var facebook_id = req.param('facebook_id');
-		} else {
-			facebook_id = null;
+			facebook_id = req.param('facebook_id');
 		}
 		
 		if (req.param('access_token')) {
-			var access_token = req.param('access_token');
-		} else {
-			access_token = null;
-		}
-		
-		// Query Facebook to see if the Facebook token is valid for the supplied Facebook ID
-		var graph_api = 'https://graph.facebook.com/me?access_token=';
-		
-		request(graph_api+access_token, function (error, response, body) {
-		
-			if (!error && response.statusCode == 200) {
+			access_token = req.param('access_token');
+		}	
 			
-				console.log(body);
+		// Check for Facebook ID and Access token params
+		if (access_token && facebook_id) {
+			
+			// Lookup DB for the User
+			var user = '';
+			
+			if (!user) {
 				
-				var result = {
-					status: 200, 
-					facebook_id: facebook_id,
-					access_token: access_token
-				};
-		
-				res.json(result, 200);
+				// User doesn't exist yet, we need to verify from Facebook this is a legit request, this is now a registration as the user
+				// was not in the DB already.
+				// Best to include the API's Facebook app secret key with these calls to verify the token originates from our app
+				
+				request.get({ url: graph_api+access_token, json: true }, function (error, response, body) {
+				
+					if (!error && response.statusCode == 200) {
+						
+						// User has a legit token
+						
+						// Perform new user insert into the DB here
+						
+						// Generate response
+						var result = {
+							status: 200,
+							message: 'Successully registered'
+						};
+						
+						// Send the success response to the client
+						res.json(result, 200);
+						
+					} else {
+					
+						// Token isn't verified, send back an error the client can use
+						return res.json({ status: 200, error: 'Facebook did not like this request' }, 200);
+						
+					}
+					
+				});
 				
 			} else {
-				return res.json({ status: 200, error: 'Facebook did not like this request' }, 200);
+				
+				// User exists already in our database
+				// Query Facebook to check the Facebook token is legit as the Facebook ID is easy to fake
+				// Best to include the API's Facebook app secret key with these calls to verify the token originates from our app
+				
+				request.get({ url: graph_api+access_token, json: true }, function (error, response, body) {
+				
+					if (!error && response.statusCode == 200) {
+						
+						// Check to see if the DB user ID matches the returned ID from Facebook
+						if (body.id == user) {
+							
+							// User has a legit token
+							
+							// Generate response
+							var result = {
+								status: 200,
+								message: 'Successully authenticated'
+							};
+							
+							// Send the success response to the client
+							res.json(result, 200);
+							
+						} else {
+							
+							// ID did not match, send back an error the client can use (not specific to the exact error)
+							return res.json({ status: 200, error: 'Facebook did not like this request' }, 200);
+							
+						}
+						
+					} else {
+					
+						// Token isn't verified, send back an error the client can use
+						return res.json({ status: 200, error: 'Facebook did not like this request' }, 200);
+						
+					}
+					
+				});
+				
 			}
 			
-		});
+		} else {
+			
+			// Insufficient params supplied, send back an error response the client can use
+			return res.json({ status: 200, error: 'Access token or user ID not specified' });
+			
+		}
 		
 	},
 
 	/**
 	* Overrides for the settings in `config/controllers.js`
-	* (specific to AuthmarkController)
+	* (specific to AuthController)
 	*/
 	_config: {}
   
